684a97599efc0bb50138a5dfa74d7752c3f4b08f
Chris Moreton <chris@netsensia.com>
2020-09-26 12:14:19 +0100

    Add more bitboard utils

--- test/rivalchess_text/core_test.clj
+++ test/rivalchess_text/core_test.clj
@@ -9,3 +9,18 @@
 (deftest northFill-test
   (testing "North Fill"
     (is (= -1332566 (northFill 4611936708517363882)))))
+
+(deftest rankBitboards-test (testing "RANK_8 is calculated correctly" (is (= -72057594037927936 RANK_8))))
+
+(deftest fileBitboards-test
+  (testing "RANK_8 is calculated correctly"
+    (is (= -9187201950435737472 FILE_A)))
+    (is (= 72340172838076673 FILE_H)))
+
+(deftest middleFiles-test
+  (testing "RANK_8 is calculated correctly"
+    (is (= 24 MIDDLE_FILES_8_BIT))
+    (is (= 231 NONMID_FILES_8_BIT))))
+
+
+
