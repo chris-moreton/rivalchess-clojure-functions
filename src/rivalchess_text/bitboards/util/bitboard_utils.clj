38f10e16513cbfb1fbc42f7c37d6d8ce926e300e
Chris Moreton <chris@netsensia.com>
2020-09-27 21:09:17 +0100

    Add pawn moves capture function to bitboard utils

--- src/rivalchess_text/bitboards/util/bitboard_utils.clj
+++ src/rivalchess_text/bitboards/util/bitboard_utils.clj
@@ -1,4 +1,5 @@
-(ns rivalchess-text.bitboards.util.bitboard_utils)
+(ns rivalchess-text.bitboards.util.bitboard_utils
+  (:use [rivalchess-text.bitboards.bitboard_constants :refer :all]))
 
 (defn orWithURightShiftedSelf [x y] (bit-or x (unsigned-bit-shift-right x y)))
 (defn orWithLeftShiftedSelf [x y] (bit-or x (bit-shift-left x y)))
@@ -6,3 +7,4 @@
 (defn northFill [x] (orWithLeftShiftedSelf (orWithLeftShiftedSelf (orWithLeftShiftedSelf x 8) 16) 32))
 (defn everyEighthBitFrom [x] (if (< x 8) (bit-shift-left 1 x) (bit-or (bit-shift-left 1 x) (everyEighthBitFrom (- x 8)))))
 (defn setBits [x] (if (empty? x) 0 (bit-or (bit-shift-left 1 (last x)) (setBits (pop x)))))
+(defn getPawnMovesCaptureOfColour [colour] (if (= colour :white) whitePawnMovesCapture blackPawnMovesCapture))
